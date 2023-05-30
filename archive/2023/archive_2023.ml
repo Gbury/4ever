0ae71cec5b156e28bc758e0423c955e4aed9886e
Guillaume Bury <guillaume.bury@gmail.com>
2023-05-31 00:17:00 +0200

    Add results for P4T 2023

--- archive/2023/archive_2023.ml
+++ archive/2023/archive_2023.ml
@@ -30,7 +30,43 @@ let winter_cup = I.ev
         ~follows:11
     ]
 
+let p4t = I.ev
+    ~log:true
+    ~name:"Printemps 4 Temps"
+    ~date:(F.Date.mk ~day:27 ~month:05 ~year:2023)
+    [
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Competitive Novice)
+        ~results:(results "p4t_novice")
+        ~leaders:36
+        ~follows:44
+      ;
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:true
+        ~category:(Competitive Intermediate)
+        ~results:(results "p4t_inter")
+        ~leaders:10
+        ~follows:15
+      ;
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Non_competitive (Some Invited))
+        ~results:(results "p4t_invite")
+        ~leaders:7
+        ~follows:8
+      ;
+    ]
+
 let events =
   List.to_seq [
     winter_cup;
+    p4t;
   ]
+
