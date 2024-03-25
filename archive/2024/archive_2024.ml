bdd70c8f028af32d24cb7f70266570cd86d0a3a3
Guillaume Bury <guillaume.bury@gmail.com>
2024-03-25 22:03:23 +0100

    4temptastiques results

--- archive/2024/archive_2024.ml
+++ archive/2024/archive_2024.ml
@@ -8,7 +8,7 @@ let results name =
   | Some contents -> I.read_results contents
 
 let winter_cup = I.ev
-    ~log:true
+    ~log:false
     ~name:"4 Temps Winter Cup"
     ~date:(F.Date.mk ~day:20 ~month:1 ~year:2024)
     [
@@ -31,8 +31,33 @@ let winter_cup = I.ev
         ~follows:14
     ]
 
+let temptastiques = I.ev
+    ~log:true
+    ~name:"4 Temptastiques"
+    ~date:(F.Date.mk ~day:24 ~month:3 ~year:2024)
+    [
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Competitive Novice)
+        ~results:(results "4ttq_novice")
+        ~leaders:31
+        ~follows:29
+      ;
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:true
+        ~category:(Competitive Intermediate)
+        ~results:(results "4ttq_inter")
+        ~leaders:6
+        ~follows:12
+    ]
+
 let events =
   List.to_seq [
     winter_cup;
+    temptastiques;
   ]
 
