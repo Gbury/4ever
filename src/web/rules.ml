af43c7616d3e1600f0011b8f6890d82c15029644
Guillaume Bury <guillaume.bury@gmail.com>
2025-01-02 22:23:33 +0100

    Updates and rules changes

--- src/web/rules.ml
+++ src/web/rules.ml
@@ -3,7 +3,9 @@ let t =
   Page.mk
     ~page:"rules"
     ~title:"Règles"
-    ~body:(fun _res _st ->
-        Rules_html.render ()
+    ~body:(fun req _st ->
+        match Misc.int_query req "ver" with
+        | Some 0 -> Rules_html.render_0 ()
+        | _ -> Rules_html.render_1 ()
       )
 
