38dab35d7cd7b6acd48844dd7348840623dab580
Guillaume Bury <guillaume.bury@gmail.com>
2025-08-18 17:54:22 +0100

    wip on utils

--- src/lib/util.ml
+++ src/lib/util.ml
@@ -1,22 +1,17 @@
 
 module Export = struct
 
-  let print l file =
-    let ch = open_out file in
-    let fmt = Format.formatter_of_out_channel ch in
-    List.iter (fun (d, points) ->
-        Format.fprintf fmt "%d\t%s\t%s@\n" points (Dancer.first_name d) (Dancer.last_name d)
-      ) l;
-    close_out ch;
-    ()
 
   let keep ~role ~div l =
     let aux d =
       let divs = Dancer.as_role ~role d in
-      match div with
-      | Division.Novice -> divs.novice
-      | Division.Intermediate -> divs.inter
-      | Division.Advanced -> divs.adv
+      let res =
+        match div with
+        | Division.Novice -> divs.novice
+        | Division.Intermediate -> divs.inter
+        | Division.Advanced -> divs.adv
+      in
+      res
     in
     List.filter aux l
 
@@ -29,16 +24,57 @@ module Export = struct
     let cmp (_, n) (_, n') = compare n' n in
     List.sort cmp l
 
-  let points_list ~role ~div ~st =
-    Dancer.search st ()
-    |> keep ~role ~div
-    |> compute_points ~st ~role ~div
-    |> sort
-
-  let inters db_file =
+  let export_inters_sorted db_file =
+    let points_list ~role ~div ~st =
+      Dancer.search st ()
+      |> keep ~role ~div
+      |> compute_points ~st ~role ~div
+      |> sort
+    in
+    let print_list l file =
+      let ch = open_out file in
+      let fmt = Format.formatter_of_out_channel ch in
+      List.iter (fun (d, points) ->
+          Format.fprintf fmt "%d\t%s\t%s@\n" points (Dancer.first_name d) (Dancer.last_name d)
+        ) l;
+      close_out ch;
+      ()
+    in
     let st = State.mk db_file in
-    print (points_list ~role:Leader ~div:Intermediate ~st) "leaders.inter";
-    print (points_list ~role:Follower ~div:Intermediate ~st) "follows.inter";
+    print_list (points_list ~role:Leader ~div:Intermediate ~st) "leaders.inter";
+    print_list (points_list ~role:Follower ~div:Intermediate ~st) "follows.inter";
+    ()
+
+  let export_num_by_points db_file =
+    let arr ~role ~div ~st =
+      let l =
+        Dancer.search st ()
+        |> keep ~role ~div
+        |> compute_points ~st ~role ~div
+      in
+      let n =
+        List.fold_left (fun acc (_, points) -> max acc points) 0 l
+      in
+      let a = Array.make (n + 1) 0 in
+      List.iter (fun (_, points) -> a.(points) <- a.(points) + 1) l;
+      a
+    in
+    let print a file =
+      let ch = open_out file in
+      let fmt = Format.formatter_of_out_channel ch in
+      Array.iteri (fun points n ->
+          Format.fprintf fmt "%d: %d@." points n
+        ) a;
+      close_out ch;
+      ()
+    in
+    let st = State.mk db_file in
+    print (arr ~role:Leader ~div:Novice ~st) "leaders.novice.txt";
+    print (arr ~role:Follower ~div:Novice ~st) "follows.novice.txt";
+    print (arr ~role:Leader ~div:Intermediate ~st) "leaders.inter.txt";
+    print (arr ~role:Follower ~div:Intermediate ~st) "follows.inter.txt";
+    print (arr ~role:Leader ~div:Advanced ~st) "leaders.adv.txt";
+    print (arr ~role:Follower ~div:Advanced ~st) "follows.adv.txt";
     ()
 
 end
