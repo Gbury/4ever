5699daba6b704edae52bccc57e397bb52b47cc88
Guillaume Bury <guillaume.bury@gmail.com>
2025-10-21 19:36:12 +0200

    [results] Spooky 4 temps

--- archive/2025/archive_2025.ml
+++ archive/2025/archive_2025.ml
@@ -98,10 +98,45 @@ let p4t = I.ev
         ~follows:8
 ]
 
+let spooky = I.ev
+    ~log:false
+    ~name:"Spooky 4 Temps"
+    ~date:(F.Date.mk ~day:19 ~month:10 ~year:2025)
+    [
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Competitive Novice)
+        ~results:(results "spooky_novice")
+        ~leaders:40
+        ~follows:46
+      ;
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Competitive Intermediate)
+        ~results:(results "spooky_inter")
+        ~leaders:27
+        ~follows:26
+      ;
+      I.comp
+        ~kind:Jack_and_Jill
+        ~name:""
+        ~check_divs:false
+        ~category:(Competitive Advanced)
+        ~results:(results "spooky_advanced")
+        ~leaders:8
+        ~follows:8
+      ;
+    ]
+
 let events =
   List.to_seq [
     winter_cup;
     ttq;
     p4t;
+    spooky;
   ]
 
